c52d0da65494395a3bfaf76f12aff3d370813762
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-04-04 11:46:48 +0100

    deposit test

--- bank_tech_test/spec/bank_spec.rb
+++ bank_tech_test/spec/bank_spec.rb
@@ -1,8 +1,12 @@
 require 'bank'
 
-  describe 'send a text' do
-    it 'sends a payment confirmation text message' do
-      allow(text).to receive(:send_text).and_return('text sent')
-      expect(text.send_text).to eq 'text sent'
+describe Bank do
+  subject(:bank) {described_class.new}
+
+  describe 'deposit' do
+    it 'can be made' do
+      bank.make_deposit('100')
+      expect(bank.account_balance).to eq ('100')
     end
   end
+end
