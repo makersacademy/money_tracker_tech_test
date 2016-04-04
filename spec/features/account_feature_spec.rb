790441f7c7a4004586cf6d545b9f4b7df73b7fd3
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-04-04 13:14:00 +0100

    added feature spec

---
+++ spec/features/account_feature_spec.rb
@@ -0,0 +1,20 @@
+describe 'Banking feature test' do
+
+  dummy_statement = {:date => "04/04/2016",
+                     :transaction => 20,
+                     :balance => -20},
+                    {:date => "04/04/2016",
+                     :transaction => 50,
+                     :balance => 30}
+
+  it 'returns correct statement' do
+    account = Account.new
+    expect(account.balance).to eq(0)
+    account.withdraw_funds(20)
+    expect(account.balance).to eq(-20)
+    account.deposit_funds(50)
+    expect(account.balance).to eq(30)
+
+    expect(account.print_statement).to eq (dummy_statement)
+  end
+end
