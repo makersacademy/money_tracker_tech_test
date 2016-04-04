aa9d8e2160ba6973eebf27ced5b5087b0e9dec38
Heather <campbell.hf@gmail.com>
2016-04-04 11:52:13 +0100

    First test passing - bank initialises with a balance.

---
+++ lib/bank.rb
@@ -0,0 +1,9 @@
+class Bank
+
+  attr_reader :balance 
+
+  def initialize
+    @balance = 0
+  end
+
+end
---
+++ spec/bank_spec.rb
@@ -0,0 +1,12 @@
+require 'bank'
+
+describe Bank do
+
+  subject(:bank) {described_class.new}
+
+  describe '#initialize' do
+    it 'intializes with a balance' do
+      expect(bank.balance).to eq 0 
+    end
+  end
+end
