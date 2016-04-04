8aa4802d3d903e1e50c0e08b817f6ddaf41927cd
innlouvate <l.j.franklin85@googlemail.com>
2016-04-04 14:42:24 +0100

    coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,10 +15,11 @@
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
 #
+require 'coveralls'
+Coveralls.wear!
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  require 'coveralls'
-  Coveralls.wear!
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
