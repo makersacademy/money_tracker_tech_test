d6b00df7e5815ef7dde6b0ef3398267a8a49abc9
Henry Gordon <hwgordon247@gmail.com>
2016-04-04 14:45:03 +0100

    refactor of the statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -14,13 +14,18 @@ class Statement
 
   def print_out
     output = "date || credit || debit || balance \n"
+    order_dates_array
+    output = iterate_dates(output)
+  end
+
+  def order_dates_array
     @dates.sort!.reverse!
+  end
+
+  def iterate_dates(output)
     @dates.each do |date|
-      if @information[date][:amount] > 0
-        output = output + generate_credit_line(date)
-      else
-        output = output + generate_debit_line(date)
-      end
+      output = output + generate_credit_line(date) if @information[date][:amount] > 0
+      output = output + generate_debit_line(date) if @information[date][:amount] < 0
     end
     output
   end
