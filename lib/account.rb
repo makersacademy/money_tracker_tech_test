b15a5778ca2bfb683c8fe60cfe90be11de910b68
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-04-04 15:07:31 +0100

    Two passing unit tests.  Third test due to pass.

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,6 @@
 class Account
+	attr_reader :balance
 	def initialize
-		
+		@balance = 0
 	end
 end
--- lib/withdrawn.rb
+++ lib/withdrawn.rb
@@ -1,5 +1,10 @@
 class Withdrawn
+	attr_reader :transactions
 	def initialize
-
+		@transactions = []
 	end
+
+	# def transactions
+	# 	transactions.dup
+	# end
 end
