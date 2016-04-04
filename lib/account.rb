cfd78ed6593f03344d2ec7ce4a6c28419e1860dd
ZeshanRasul <zeshanrasul90@gmail.com>
2016-04-04 15:26:50 +0100

    Functionality except print completed, beginning to extract to classes for SRP

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,26 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :previous_transactions, :current_transaction
 
   def initialize
     @balance = 0
+    @previous_transactions = []
+    # @current_transaction = {}
   end
 
   def accept_deposit(amount)
     @balance += amount
+    time_to_save = Time.now.strftime("%d/%m/%Y")
+    transaction = {time_to_save => amount}
+    @previous_transactions << transaction
   end
 
   def accept_withdrawal(amount)
     @balance -= amount
+    time_to_save = Time.now.strftime("%d/%m/%Y")
+    transaction = {time_to_save => -amount}
+    @previous_transactions << transaction
   end
+
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,6 +7,14 @@ describe Account do
       expect(subject.balance).to eq 0
     end
 
+    it 'contains an empty array of previous deposits' do
+      expect(subject.previous_transactions).to eq []
+    end
+
+    # it 'contains an empty hash for the current transaction' do
+    #   expect(subject.current_transaction).to eq {}
+    # end
+
     it { is_expected.to respond_to(:accept_deposit).with(1).argument }
 
     it 'changes the balance after deposits' do
@@ -14,10 +22,35 @@ describe Account do
       expect(subject.balance).to eq 2000
     end
 
+
+    it 'saves the amount and date when a deposit is made' do
+      subject.accept_deposit(1000)
+      current_time = Time.now.strftime("%d/%m/%Y")
+      resulting_hash = {current_time => 1000}
+      expect(subject.previous_transactions).to eq [resulting_hash]
+    end
+
     it { is_expected.to respond_to(:accept_withdrawal).with(1).argument }
 
     it 'changes the balance after withdrawals' do
       subject.accept_withdrawal(500)
       expect(subject.balance).to eq -500
+    end
+
+    it 'saves the amount and date when a withdrawal is made' do
+      subject.accept_withdrawal(1000)
+      current_time = Time.now.strftime("%d/%m/%Y")
+      resulting_hash = {current_time => -1000}
+      expect(subject.previous_transactions).to eq [resulting_hash]
+    end
+
+    it 'saves the current transaction into an array of previous transactions' do
+      p subject.accept_deposit(5000)
+      p subject.accept_withdrawal(1000)
+      current_time = Time.now.strftime("%d/%m/%Y")
+      # deposit_hash = {current_time => 5000}
+      # withdrawal_hash = {current_time => -1000}
+      # previous_trans_array = [deposit_hash, withdrawal_hash]
+      expect(subject.previous_transactions).to eq [{Time.now.strftime("%d/%m/%Y") => 5000}, {Time.now.strftime("%d/%m/%Y") => -1000}]
     end
 end
