f56aa16f470a5ced4ca88f9dd4be3fee659478ab
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-04-04 14:36:32 +0100

    presentation changed

--- bank_tech_test/lib/account.rb
+++ bank_tech_test/lib/account.rb
@@ -1,5 +1,5 @@
 
-class Bank
+class Account
   attr_reader :account_balance, :account_statement
 
   def initialize
@@ -9,12 +9,12 @@ class Bank
 
   def make_deposit(deposit)
     @account_balance = account_balance + deposit
-    account_statement.push({:date => Time.now.strftime("%m/%d/%Y")}, {:credit => deposit}, {:balance => @account_balance})
+    account_statement.push({:date => Time.now.strftime("%m/%d/%Y"), :credit => deposit, :balance => @account_balance})
   end
 
   def make_withdrawal(withdrawal)
     @account_balance = account_balance - withdrawal
-    account_statement.push({:date => Time.now.strftime("%m/%d/%Y")}, {:debit => withdrawal}, {:balance => @account_balance})
+    account_statement.push({:date => Time.now.strftime("%m/%d/%Y"), :debit => withdrawal, :balance => @account_balance})
   end
 
   def print_statement
--- bank_tech_test/spec/bank_spec.rb
+++ bank_tech_test/spec/bank_spec.rb
@@ -1,50 +1,49 @@
-require 'bank'
+require 'account'
 
-describe Bank do
-  subject(:bank) {described_class.new}
+describe Account do
+  subject(:account) {described_class.new}
 
   describe 'balance' do
     it 'balance starts at 0' do
-      expect(bank.account_balance).to eq 0
+      expect(account.account_balance).to eq 0
     end
   end
 
   describe 'deposit' do
     it 'can be made' do
-      bank.make_deposit(100)
-      expect(bank.account_balance).to eq(100)
+      account.make_deposit(100)
+      expect(account.account_balance).to eq(100)
     end
   end
 
   describe 'withdrawal' do
     it 'can be made' do
-      bank.make_deposit(200)
-      bank.make_withdrawal(100)
-      expect(bank.account_balance).to eq(100)
+      account.make_deposit(200)
+      account.make_withdrawal(100)
+      expect(account.account_balance).to eq(100)
     end
   end
 
   describe 'account balance' do
     it 'can be viewed' do
-      bank.make_deposit(400)
-      bank.make_deposit(100)
-      bank.make_withdrawal(35)
-      expect(bank.account_balance).to eq(465)
+      account.make_deposit(400)
+      account.make_deposit(100)
+      account.make_withdrawal(35)
+      expect(account.account_balance).to eq(465)
     end
 
     it 'can be printed' do
-      bank.make_deposit(400)
-      bank.make_deposit(100)
-      bank.make_withdrawal(35)
-      expect(bank.print_statement).to eq [{:date => "04/04/2016"}, {:credit => 400}, {:balance => 400}, {:date => "04/04/2016"}, {:credit => 100}, {:balance => 500}, {:date => "04/04/2016"}, {:debit => 35}, {:balance => 465}]
+      account.make_deposit(400)
+      account.make_deposit(100)
+      account.make_withdrawal(35)
+      expect(account.print_statement).to eq [{:date => "04/04/2016"}, {:credit => 400}, {:balance => 400}, {:date => "04/04/2016"}, {:credit => 100}, {:balance => 500}, {:date => "04/04/2016"}, {:debit => 35}, {:balance => 465}]
     end
   end
 
   describe 'date' do
     it 'is attached to statement' do
-      bank.make_deposit(400)
-      expect(bank.account_statement).to include({:date=>"04/04/2016"})
+      account.make_deposit(400)
+      expect(account.account_statement).to include({:date=>"04/04/2016"})
     end
   end
-
 end
