7e170342506e3b04ff907651204067600f0aa000
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-04-04 12:30:56 +0100

    added calculate statement method

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,30 @@
 class Account
-  attr_reader :balance
+  attr_reader :balance, :account_statement
 
   def initialize
     @balance = 0
+    @account_statement = []
   end
 
   def withdraw_funds(amount)
     @balance -= amount
+    calculate_statement(amount)
   end
 
   def deposit_funds(amount)
     @balance += amount
+    calculate_statement(amount)
+  end
+
+  def calculate_statement(amount)
+    statement = {}
+    statement[:date] = time
+    statement[:transaction] = amount
+    statement[:balance] = @balance
+    account_statement.push(statement)
+  end
+
+  def time
+    Time.now.strftime("%m/%d/%Y")
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,4 +14,15 @@ describe Account do
       expect{account.deposit_funds(20)}.to change{account.balance}.by(20)
     end
   end
+
+  describe '#calculate_statement' do
+    it 'returns the correct account statement' do
+      dummy_statement = {
+      :date => "04/04/2016",
+      :transaction => 20,
+      :balance => 0,
+    }
+      expect(account.calculate_statement(20)).to include(dummy_statement)
+    end
+  end
 end
