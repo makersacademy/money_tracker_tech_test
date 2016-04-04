221157f99e73570d1e99b5106406883a79589b37
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-04-04 11:38:57 +0100

    added simple withdraw method

---
+++ lib/account.rb
@@ -0,0 +1,11 @@
+class Account
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def withdraw_funds(amount)
+    @balance -= amount
+  end
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Account do
+  subject(:account) {described_class.new}
+
+  describe '#withdraw' do
+    it 'allows user to withdraw funds' do
+      expect{account.withdraw_funds(20)}.to change{account.balance}.by(-20)
+    end
+  end
+end
