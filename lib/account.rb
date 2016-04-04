938f95dcbeb022562f86f2dc31128c2ac2d634c0
arnoldmanzano <arnold.manzano@gmail.com>
2016-04-04 11:49:14 +0100

    Simple deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,13 @@
 class Account
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
+  end
+
+  def deposit(amount, date)
+    @balance += amount
+    @transactions.push([amount, date])
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,8 +4,22 @@ describe Account do
   subject(:account) {described_class.new}
 
   describe '#initialize and #balance' do
-    it 'initialize with 0 balance' do
+    it 'initialize with 0 balance and no transactions' do
       expect(account.balance).to eq 0
+      expect(account.transactions).to be_empty
+    end
+  end
+
+  describe '#deposit' do
+    it 'can make a deposit' do
+      amount = 1000
+      expect{account.deposit(amount, '10-01-2012')}
+        .to change{account.balance}.by amount
+    end
+
+    it 'makes a record of the transaction' do
+      account.deposit(1000, '10-01-2012')
+      expect(account.transactions).to include [1000, '10-01-2012']
     end
   end
 end
