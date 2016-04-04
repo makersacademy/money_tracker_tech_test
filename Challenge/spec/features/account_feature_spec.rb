61a63d2ee8d063714e83fbc160446395c4e726b3
JoeWroe <joe_wroe@icloud.com>
2016-04-04 16:10:19 +0100

    DEPOSIT REFACTOR
    Refactored the tests to move inslightly smaller steps.
    The date feature on the '#deposit' method is now redundant, but I feel it will be important later so I have left it in place.

--- Challenge/spec/features/account_feature_spec.rb
+++ Challenge/spec/features/account_feature_spec.rb
@@ -4,13 +4,6 @@ describe 'An Account' do
 
   describe 'has a balance that' do
 
-    it 'can be printed out in a statement' do
-      account = Account.new
-      account.deposit('10/01/2012', 1000)
-      expect(account.print_statement).to eq('date || credit || debit || balance
-                                             10/01/2012 || 1000.00 || || 1000.00')
-    end
-
     it 'can be altered with deposits' do
       account = Account.new
       account.deposit('10/01/2012', 1000)
@@ -19,6 +12,13 @@ describe 'An Account' do
 
     xit 'can be altered with withdrawals' do
 
+    end
+
+    describe 'has a statement feature that' do
+
+      it 'prints out a table' do
+        expect(account.print_statement).to eq('date || credit || debit || balance')
+      end
     end
   end
 
