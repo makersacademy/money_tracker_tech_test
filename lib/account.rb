e8fabd2b6331a36c8de1e82e38b12443e18b58c2
rachelsmithcode <rachelcodesmith@outlook.com>
2016-04-04 13:51:55 +0100

    tests pass with basic functionality. Needs refactor, class extraction and for date to display 0 in front of days that are < 10

--- lib/account.rb
+++ lib/account.rb
@@ -1,19 +1,55 @@
+require 'date'
+
 class Account
 
   def initialize
-    @balance = 0
+    @balance = 0.00
+    @history = []
   end
 
   def show_balance
-    @show_balance = @balance
+    sprintf( "%0.02f", @balance)
   end
 
-  def deposit(amount)
-    @balance += amount
+  def show_statement
+    statement = @history.map do |item|
+      "#{item[:date]} || #{item[:deposit]} || #{item[:withdrawal]} || #{item[:balance]}"
+    end
+    statement.unshift("Date || Credit || Debit || Balance")
   end
 
-  def withdraw(amount)
-    @balance -= amount
+  def make_deposit(amount)
+    deposit(amount)
+    @history << {deposit: sprintf( "%0.02f", amount), withdrawal: '', date: todays_date, balance: show_balance}
   end
+
+  def make_withdrawal(amount)
+    withdraw(amount)
+    @history << {deposit: '', withdrawal: sprintf( "%0.02f", amount), date: todays_date, balance: show_balance}
+  end
+
+  def todays_date
+    get_date
+    format_date
+  end
+
+
+  private
+
+    def deposit(amount)
+      @balance += amount
+    end
+
+    def withdraw(amount)
+      @balance -= amount
+    end
+
+    def format_date
+      "#{@date.day}/#{@date.month}/#{@date.year}"
+    end
+
+    def get_date
+      @date = Date.today
+    end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,10 +3,14 @@ require "./lib/account.rb"
 describe Account do
   subject(:account) {described_class.new}
 
+  before do
+    allow(account).to receive(:todays_date).and_return("4/4/2016")
+  end
+
   describe "#initialize" do
 
     it "initializes a new account with a balance of 0" do
-      expect(account.show_balance).to eq 0
+      expect(account.show_balance).to eq "0.00"
     end
 
   end
@@ -16,9 +20,14 @@ describe Account do
     describe "#deposit" do
 
       it "adds the amount deposited to the balance" do
-        account.deposit(5.00)
-        expect(account.show_balance).to eq 5.00
+        account.make_deposit(5.00)
+        expect(account.show_balance).to eq "5.00"
       end
+
+      # it "saves the date a deposit is made to the history array" do
+      #   account.make_deposit(5.0)
+      #   expect(account.show_statement).to eq [{amount: 5.0, date: "4/4/2016"}]
+      # end
 
     end
 
@@ -29,12 +38,44 @@ describe Account do
     describe "#withdrawal" do
 
       it "subtracts the amount withdrawn from the balance" do
-        account.withdraw(5.00)
-        expect(account.show_balance).to eq -5.00
+        account.make_withdrawal(5.00)
+        expect(account.show_balance).to eq "-5.00"
+      end
+
+      # it "saves the date a withdrawal is made to the history array" do
+      #   account.make_withdrawal(-5.0)
+      #   expect(account.show_statement).to eq [{amount: -5.0, date: "4/4/2016"}]
+      # end
+
+    end
+
+  end
+
+  context "display statement history" do
+
+    describe "#show_statement" do
+
+      it "properly displays the history after a deposit of 5.00 is made" do
+        account.make_deposit(5.00)
+        expect(account.show_statement).to eq ["Date || Credit || Debit || Balance", "4/4/2016 || 5.00 ||  || 5.00"]
+      end
+
+      it "properly displays the history after a withdrawal of 5.00 is made" do
+        account.make_withdrawal(5.00)
+        expect(account.show_statement).to eq ["Date || Credit || Debit || Balance", "4/4/2016 ||  || 5.00 || -5.00"]
+      end
+
+      it "records and properly displays the history of multiple transactions" do
+        account.make_deposit(5.00)
+        account.make_withdrawal(5.00)
+        expect(account.show_statement).to eq ["Date || Credit || Debit || Balance", "4/4/2016 || 5.00 ||  || 5.00", "4/4/2016 ||  || 5.00 || 0.00"]
       end
 
     end
 
   end
+
+
+
 
 end
