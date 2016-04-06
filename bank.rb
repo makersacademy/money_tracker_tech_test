9f711384d6fbb6c686268c24569e1230f36a3550
Kait Beaudette <kait@kaitmakesthings.com>
2016-04-06 13:40:05 -0500

    Refactor Bank.new to Rspec subject syntax, Add balance display to statement

--- bank.rb
+++ bank.rb
@@ -1,22 +1,19 @@
 class Bank
 
-  attr_reader :statement
+  attr_reader :statement, :balance
 
-  def initialize
-    @balance = 0
+  def initialize(balance=0)
+    @balance = balance
     @statement = []
-  end
-
-  def balance
-    @balance
   end
 
   def deposit(amount)
     @balance += amount
-    @statement << @balance
+    @statement.push("Deposit:#{amount}, Balance:#{@balance}")
   end
   
   def withdraw(amount)
     @balance -= amount
+    @statement.push("Withdrawal:#{amount}, Balance:#{@balance}")
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,4 +1,5 @@
 describe Bank do
+  subject {described_class .new}
 
   it {is_expected.to be}
   it {is_expected.to respond_to(:balance)}
@@ -7,33 +8,45 @@ describe Bank do
   it {is_expected.to respond_to(:statement)}
 
   describe ".balance" do
-    my_account = Bank.new
     it "has a starting balance of zero dollars" do
-      expect(my_account.balance).to eq(0)
+      expect(subject.balance).to eq(0)
     end
   end
 
   describe ".deposit" do
     it "adds money to the bank account" do
-      my_account = Bank.new
-      expect{my_account.deposit(100)}.to change {my_account.balance}.by(100)
+      expect{subject.deposit(100)}.to change {subject.balance}.by(100)
     end
   end
 
   describe ".withdraw" do
     it "removes money from the bank account" do
-      my_account = Bank.new
-      expect{my_account.withdraw(100)}.to change {my_account.balance}.by(-100)
+      expect{subject.withdraw(100)}.to change {subject.balance}.by(-100)
     end
   end
 
   describe ".statement" do
+    it "displays the current balance after each transaction" do
+      subject.deposit(100)
+      subject.withdraw(100)
+      expect(subject.statement.first).to match(/Balance:100/)
+      expect(subject.statement.last).to match(/Balance:0/)
+    end
+
     context "after a deposit" do
       it "shows the deposit amount" do
-        my_account = Bank.new
-        my_account.deposit(100)
-        expect(my_account.statement).to include(100)
+        subject.deposit(100)
+        expect(subject.statement.last).to match(/Deposit:100/)
+      end
+    end
+
+    context "after a withdrawal" do
+      it "shows the withdrawal amount" do
+        subject.withdraw(100)
+        expect(subject.statement.last).to match(/Withdrawal:100/)
       end
     end  
   end
+
+
 end
