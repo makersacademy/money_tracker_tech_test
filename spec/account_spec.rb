95dfe1af28b56c0829ff7b56ba9b431a2a682053
Annemarie Kohler <annemarie.k89@gmail.com>
2016-05-16 15:58:10 +0100

    working

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,13 @@ describe BankAccount do
   subject(:account) { described_class.new }
   # let(:amount) { double 100 }
   let(:new_date) { double :new_date }
-  let(:transaction_instance) { double :transaction, amount: 100, date: :new_date }
+  let(:transaction_instance1) { double :transaction,
+                                credit: 3000,
+                                date: :new_date }
+
+  let(:transaction_instance2) { double :transaction,
+                                debit: 500,
+                                date: :new_date }
 
   it 'has an array of transactions' do
     expect(account.transactions).to eq []
@@ -20,8 +26,8 @@ describe BankAccount do
     it { is_expected.to respond_to(:deposit).with(1).argument }
 
     it 'returns the amount that has been put into the account' do
-      new_balance = account.balance + transaction_instance.amount
-      account.deposit(transaction_instance)
+      new_balance = account.balance + transaction_instance1.credit
+      account.deposit(transaction_instance1)
       expect(account.current_balance).to eq(new_balance)
     end
   end
@@ -30,8 +36,8 @@ describe BankAccount do
     it { is_expected.to respond_to(:withdraw).with(1).argument }
 
     it 'returns the amount that has been taken out of the account' do
-      new_balance = account.balance - transaction_instance.amount
-      account.withdraw(transaction_instance)
+      new_balance = account.balance - transaction_instance2.debit
+      account.withdraw(transaction_instance2)
       expect(account.current_balance).to eq(new_balance)
     end
   end
