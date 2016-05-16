4bdaa425b2310bf8094b575a1bf34c2befd331c9
Murilo Dal Ri <murilodalri@gmail.com>
2016-05-16 12:29:17 +0100

    a few edge case tests added

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,10 @@
 class Account
   STARTING_BALANCE = 100
 
-  def initialize(balance: STARTING_BALANCE)
+  def initialize(log: TransactionHistory.new, balance: STARTING_BALANCE, pin:1234)
     @balance = balance
+    @pin = pin
+    @log = log
   end
 
   attr_reader :balance
@@ -11,8 +13,8 @@ class Account
     @balance += amount if valid_deposit(amount)
   end
 
-  def withdraw(amount)
-    @balance -= amount if valid_withdraw(amount)
+  def withdraw(amount, pin)
+    @balance -= amount if valid_withdraw(amount) && valid_pin(pin)
   end
 
   private
@@ -23,5 +25,9 @@ class Account
 
   def valid_withdraw(amount)
     (amount.is_a? Numeric) && amount > 0 && amount <= @balance
+  end
+
+  def valid_pin(pin)
+    @pin == pin
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,13 +7,23 @@ describe Account do
     expect(account.balance).to eq Account::STARTING_BALANCE
   end
 
-  it 'Can make deposits' do
+  it 'Can make valid deposit' do
     account.make_deposit 50
     expect(account.balance).to eq Account::STARTING_BALANCE + 50
   end
 
-  it 'Can withdraw' do
-    account.withdraw 40
+  it 'Can make valid withdraw' do
+    account.withdraw(40, 1234)
     expect(account.balance).to eq Account::STARTING_BALANCE - 40
+  end
+
+  it 'Cannot make invalid deposit' do
+    account.make_deposit(-50)
+    expect(account.balance).to eq Account::STARTING_BALANCE
+  end
+
+  it 'Cannot make withdraw with wrong pin' do
+    account.withdraw(40, 4321)
+    expect(account.balance).to eq Account::STARTING_BALANCE
   end
 end
