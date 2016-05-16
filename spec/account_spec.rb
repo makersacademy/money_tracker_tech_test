d77b1255a590769215f7314f0899ff743a17070e
Annemarie Kohler <annemarie.k89@gmail.com>
2016-05-16 11:33:42 +0100

    refactored

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,7 @@
 require 'account'
 
 describe BankAccount do
-  it 'should initialise with a balance of 0' do
+  it 'initialises with a balance of 0' do
     expect(subject.balance).to eq(0)
   end
 end
