b22d43d32c75708ac2532132cdbd1a9ed5a20ab8
eripheebs <erikapheby@gmail.com>
2016-05-16 11:12:25 +0100

    withdraw method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -12,8 +12,13 @@ describe Account do
   describe '#deposit' do
     it 'adds amount to balance' do
       RANDOM_AMOUNT = rand(10)
-      account.deposit(RANDOM_AMOUNT)
-      expect(account.balance).to eq(RANDOM_AMOUNT)
+      expect{account.deposit(RANDOM_AMOUNT)}.to change{account.balance}.by(RANDOM_AMOUNT)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'deducts amount from balance' do
+      expect{account.withdraw(RANDOM_AMOUNT)}.to change{account.balance}.by(-RANDOM_AMOUNT)
     end
   end
 end
