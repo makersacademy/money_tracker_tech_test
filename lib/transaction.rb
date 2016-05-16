a2c73bc349787fe17c59b847848491f02704111b
eripheebs <erikapheby@gmail.com>
2016-05-16 11:29:44 +0100

    transaction class initializes with credit and debit

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,11 @@
 class Transaction
 
-  attr_reader :date
+  attr_reader :date, :credit, :debit
 
-  def initialize(date)
+  def initialize(date, credit=0, debit=0)
     @date = date
+    @credit = credit
+    @debit = debit
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,11 +2,21 @@ require 'transaction'
 
 describe Transaction do
   DATE = Date.new(2016,5,16)
-  let(:transaction) { described_class.new DATE}
+  CREDIT = rand(1000)
+  DEBIT = rand(1000)
+  let(:transaction) { described_class.new DATE, CREDIT, DEBIT}
 
   describe '#initialize' do
     it 'has a date' do
       expect(transaction.date).to eq(DATE)
+    end
+
+    it 'has a credited amount' do
+      expect(transaction.credit).to eq(CREDIT)
+    end
+
+    it 'has a debited amount' do
+      expect(transaction.debit).to eq(DEBIT)
     end
   end
 
