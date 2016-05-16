1c7fc10702f0b7d4f209e84fbe82da2f01e8268b
eripheebs <erikapheby@gmail.com>
2016-05-16 10:53:21 +0100

    has balance of 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,12 @@
 require 'account'
 
 describe Account do
+  let(:account) { described_class.new }
 
-  
+  describe '#initialize' do
+    it 'has a balance of 0' do
+      expect(account.balance).to eq(0)
+    end
+  end
 
 end
