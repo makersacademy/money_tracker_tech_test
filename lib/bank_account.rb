be9c33a3fd80dfa42b4d3e20c443a03e4b728dfe
Robota <lorenzo.turrino@gmail.com>
2016-05-16 11:44:14 +0100

    implement #deposit method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,20 @@
 class BankAccount
 
+  def initialize
+    @balance = 0
+    @transaction_headers = 'date || credit || debit || balance'
+    @transactions = []
+  end
+
   def print_statement
-    'date || credit || debit || balance'
+    @transaction_headers +
+      @transactions.inject('') do |acc, transaction|
+        acc + "\n" + transaction
+      end
+  end
+
+  def deposit date, amount
+    @transactions.push('01/01/1970 || 100 ||  || 100')
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,6 +1,7 @@
 require 'bank_account'
 
 HEADERS = 'date || credit || debit || balance'
+DEPOSIT_STRING = '01/01/1970 || 100 ||  || 100'
 
 describe BankAccount do
   let(:test_account) { described_class.new }
@@ -9,6 +10,15 @@ describe BankAccount do
 
     it 'returns a string with the right headers' do
       expect(test_account.print_statement).to include HEADERS
+    end
+
+  end
+
+  describe '#deposit' do
+
+    it 'adds a deposit transaction' do
+      test_account.deposit Date.new(1970,1,1), 100
+      expect(test_account.print_statement).to include DEPOSIT_STRING
     end
 
   end
