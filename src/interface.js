8feeda3e2aba572d87352ee39d58f918b05b32ff
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-16 17:43:54 +0100

    can deposit, withdraw and print statement in jquery

--- src/interface.js
+++ src/interface.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
+  var bankAccount = new BankAccount(Transaction);
 
-}
+  $('#deposit').click(function () {
+    var amount = $('#amount').val();
+    bankAccount.deposit(parseInt(amount));
+  });
+
+  $('#withdraw').click(function () {
+    var amount = $('#amount').val();
+    bankAccount.withdraw(parseInt(amount));
+  });
+
+  $('#print-statement').click(function () {
+    var statement = new Statement(bankAccount);
+    var output = statement.print();
+    $('#statement').text(output);
+  });
+});
