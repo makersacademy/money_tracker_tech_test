8d61294b44a24cedc19bc036359e7c056e7ac5a4
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-05-16 17:45:04 +0100

    bigDecimal implemented

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 require 'account'
 
 describe Account do
@@ -7,38 +8,36 @@ describe Account do
   DATE1 = Date.new(2012,1,10)
   DATE2 = Date.new(2012,1,13)
   subject(:account) { described_class.new }
-  let(:transaction_class) { double(:transaction_class) }
+  let(:transaction_class) { double :transaction_class }
 
   describe '#deposit' do 
-
     it 'will add the balance passed to deposit to the balance' do
-      account.deposit(DEPOSIT_AMOUNT1, DATE1)
+      account.deposit(amount: DEPOSIT_AMOUNT1, date: DATE1)
       expect(account.balance).to eq DEPOSIT_AMOUNT1
     end
 
     it 'will create a transaction object' do
-      allow(transaction_class).to receive(:new)
-      account.deposit(DEPOSIT_AMOUNT1, DATE1)
-      expect(transaction_class).to receive(:new).with(credit: DEPOSIT_AMOUNT1, date: DATE1, balance: DEPOSIT_AMOUNT1)
-      account.deposit(DEPOSIT_AMOUNT1, DATE1)
+      allow(transaction_class).to receive(:new).with(date: DATE1, credit: DEPOSIT_AMOUNT1, balance: DEPOSIT_AMOUNT1)
+      account.deposit(amount: DEPOSIT_AMOUNT1, date: DATE1)
+      expect(transaction_class).to receive(:new).with(date: DATE1, credit: DEPOSIT_AMOUNT1, balance: DEPOSIT_AMOUNT1)
+      account.deposit(amount: DEPOSIT_AMOUNT1, date: DATE1)
     end
 
     it 'will add the transaction  to the statement' do
-      account.deposit(DEPOSIT_AMOUNT1, DATE1)
+      account.deposit(amount: DEPOSIT_AMOUNT1, date: DATE1)
       expect(account.statement.length).to eq 1
     end
   end
 
   describe '#withdraw' do
-    
     it 'will deduct the balance passed to deposit from the balance' do
-      account.deposit(DEPOSIT_AMOUNT1, DATE1)
-      account.withdraw(WITHDRAWAL_AMOUNT1, DATE2)
+      account.deposit(amount: DEPOSIT_AMOUNT1, date: DATE1)
+      account.withdraw(amount: WITHDRAWAL_AMOUNT1, date:DATE2)
       expect(account.balance).to eq DEPOSIT_AMOUNT1 - WITHDRAWAL_AMOUNT1
     end
 
     it 'will add the transaction to the statement' do
-      account.withdraw(WITHDRAWAL_AMOUNT1, DATE2)
+      account.withdraw(amount: WITHDRAWAL_AMOUNT1, date:DATE2)
       expect(account.statement.length).to eq 1
     end
   end
@@ -57,8 +56,8 @@ describe Account do
 
   describe '#print_statement' do
     it 'will print to the screen' do
-      account.deposit(DEPOSIT_AMOUNT1, DATE1)
-      account.withdraw(WITHDRAWAL_AMOUNT1, DATE2)
+      account.deposit(amount: DEPOSIT_AMOUNT1, date: DATE1)
+      account.withdraw(amount: WITHDRAWAL_AMOUNT1, date: DATE2)
       expect { account.print_statement }.to output.to_stdout
     end
   end
--- spec/bank_feature_test.rb
+++ spec/bank_feature_test.rb
@@ -12,14 +12,14 @@ p "transaction balance"
 p t.balance
 p a = Account.new
 p "deposit"
-p a.deposit(1000,Date.new(2012,1,10))
+p a.deposit(amount: 1000,date: Date.new(2012,1,10))
 p "balance"
 p a.balance
 p "withdraw"
-p a.deposit(2000,Date.new(2012,1,13))
+p a.deposit(amount: 2000, date: Date.new(2012,1,13))
 p "balance"
 p a.balance
-p a.withdraw(500,Date.new(2012,1,14))
+p a.withdraw(amount: 500, date: Date.new(2012,1,14))
 p "next one is statement"
 p a.statement
 
