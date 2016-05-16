6831f673ff49eb6a026604a29e672a99aa3fd5e3
Caspar Fischer-Zernin <casparfz@gmail.com>
2016-05-16 11:11:50 +0100

    added deposit and withdraw function

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,39 @@
 class Account
 
-  def balance
-    0
+STARTBALANCE = 10
+
+  def initialize
+    @balance = STARTBALANCE
   end
 
-  def statement
+  def check_balance
+    balance
   end
+  
+  def statement
+    []
+  end
+
+  def deposit(amount)
+    add_to_balance(amount)
+  end
+
+  def withdraw(amount)
+    subtract_from_balance(amount)
+  end
+
+  private
+
+  def balance
+    @balance
+  end
+
+  def add_to_balance(amount)
+    @balance += amount
+  end
+
+  def subtract_from_balance(amount)
+    @balance -= amount
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,12 +1,35 @@
 require 'account'
 describe Account do
+  let(:start_balance) {Account::STARTBALANCE}
   let(:account) {described_class.new}
-  describe '#balance' do
-    it 'should know the balance' do
-      expect(account.balance).to be_a(Integer)
+  
+  it 'should initialize with the STARTBALANCE' do
+    expect(account.check_balance).to eq(start_balance)
+  end
+
+  describe '#check_balance' do
+    it 'should return the balance' do
+      expect(account.check_balance).to be_a(Integer)
     end
   end
+  
   describe '#statement' do
     it {is_expected.to respond_to(:statement)}
+
+    it 'should be an array' do
+      expect(account.statement).to be_a(Array)
+    end
+  end
+
+  describe '#desposit' do
+    it 'should add money to balance' do
+      expect{account.deposit(100)}.to change{account.check_balance}.by(100)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'should add money to balance' do
+      expect{account.withdraw(50)}.to change{account.check_balance}.by(-50)
+    end
   end
 end
