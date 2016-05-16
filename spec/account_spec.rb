cde192cff4961d5cfb0c2cad41cbfe2b6380f0c0
Joseph Sharratt <joseph.sharratt@gmail.com>
2016-05-16 16:28:58 +0100

    implementing statement

---
+++ spec/account_spec.rb
@@ -0,0 +1,70 @@
+require 'account'
+
+describe Account do
+
+  let(:credit) { double :transaction, amount: 1000, date: '10-01-2012' }
+  let(:credit2) { double :transaction, amount: 2000, date: '13-01-2012' }
+  let(:debit) { double :transaction, amount: -500, date: '14-01-2012' }
+
+
+  describe 'initialize' do
+    it 'is initialized with a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+
+    it 'is initialized with an empty transactions array' do
+      expect(subject.transactions).to eq []
+    end
+  end
+
+  describe 'transact' do
+
+    before(:each) do
+      subject.transact(credit)
+      subject.transact(credit2)
+    end
+
+    it 'records a deposit in transactions history' do
+      expect(subject.transactions).to include(credit)
+    end
+
+    it 'balance is increased by deposited amount ' do
+      expect(subject.balance).to eq 3000
+    end
+
+    describe 'withdrawal' do
+
+      before(:each) do
+        subject.transact(debit)
+      end
+
+      it 'records a withdrawal in transactions history ' do
+        expect(subject.transactions).to include(debit)
+      end
+
+      it 'balance is reduced by withdrawn amount' do
+        expect(subject.balance).to eq 2500
+      end
+    end
+
+  end
+
+
+  describe 'statement' do
+
+    before(:each) do
+      subject.transact(credit)
+      subject.transact(credit2)
+      subject.transact(debit)
+    end
+
+    it 'prints out correctly formatted statement' do
+      expect(subject.statement).to eq(
+      "date || credit || debit || balance
+      14/01/2012 || || 500.00 || 2500.00
+      13/01/2012 || 2000.00 || || 3000.00
+      10/01/2012 || 1000.00 || || 1000.00")
+    end
+  end
+
+end
---
+++ user_stories.md
@@ -0,0 +1,29 @@
+As bank's client
+So that I can safely store my money
+I would like to be able to to deposit a given amount in my account
+
+As a client
+So that I can later use money I have deposited
+I would like to be able to withdraw a given amount from my account
+
+As a client
+So that I can make sense of historic deposits
+I would like the date to be recorded with each deposit I make
+
+As a client
+So that I can keep track of my in and outgoing money
+I would like to be able to print out a bank statement of my account
+
+
+
+
+Nouns:
+
+Client(User)
+bank account
+transaction
+
+Verbs:
+
+deposit
+withdraw
