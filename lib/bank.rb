cde497f78f6379772bcdd43c41e42dadb12e26e0
Misa Ogura <misa.ogura01@gmail.com>
2016-05-16 15:06:39 +0100

    Complete implementing print_statement method

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,12 +2,18 @@ class Bank
 
   def print_statement(account)
     history = account.history
-    statement = "date || credit || debit || balance\n"
-    # history.each do ||
-
-
-
-
+    format_transaction(history)
   end
 
+  private
+
+  def format_transaction(history)
+    statement = "date || credit || debit || balance\n"
+    history.each do |transaction|
+      amount = transaction.amount
+      amount > 0 ? credit = amount : debit = amount.abs
+      statement += "#{transaction.date} || #{credit} || #{debit} || #{transaction.balance}\n"
+    end
+    statement
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,23 +4,22 @@ describe Bank do
 
   subject(:bank) { described_class.new }
 
-  let(:transaction1) { double(:transaction, date: '10/01/2012', amount: -1000) }
-  let(:transaction2) { double(:transaction, date: '15/01/2012', amount: 2500) }
-  let(:history) { [{transaction: transaction1, balance: -1000},
-                   {transaction: transaction1, balance:  1500}] }
+  let(:transaction1) { double(:transaction, date: '10/01/2012', amount: -1000, balance: -1000) }
+  let(:transaction2) { double(:transaction, date: '15/01/2012', amount: 2500, balance:  1500) }
+  let(:history) { [transaction1, transaction2] }
 
-  let(:empty_account) { double(:account, balance: 0) }
+  let(:empty_account) { double(:account, balance: 0, history: []) }
   let(:used_account) { double(:account, balance: 1500, history: history) }
 
-  # describe '#print_statement' do
-  #   it 'prints out a statement with date, credit, deposit and balance' do
-  #     colums = 'date || credit || debit || balance'
-  #     expect(bank.print_statement(empty_account).include?(colums)).to eq true
-  #   end
-  #
-  #   it 'prints out a statement with date, credit, deposit and balance' do
-  #     statement = bank.print_statement(used_account)
-  #     expect(statement.include?('10/01/2012' && '-1000' && '15/01/2012' && '2500')).to eq true
-  #   end
-  # end
+  describe '#print_statement' do
+    it 'prints out a statement with date, credit, deposit and balance' do
+      colums = 'date || credit || debit || balance'
+      expect(bank.print_statement(empty_account).include?(colums)).to eq true
+    end
+
+    it 'prints out a statement with date, credit, deposit and balance' do
+      statement = bank.print_statement(used_account)
+      expect(statement.include?('10/01/2012' && '-1000' && '15/01/2012' && '2500')).to eq true
+    end
+  end
 end
