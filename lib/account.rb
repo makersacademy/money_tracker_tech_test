039986fde528616deb2fb5297096339922daa0cc
Murilo Dal Ri <murilodalri@gmail.com>
2016-05-16 11:03:52 +0100

    refactored

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,11 @@
 class Account
   STARTING_BALANCE = 100
+
+  def initialize(balance: STARTING_BALANCE)
+    @balance = balance
+  end
+
   def get_balance
-    STARTING_BALANCE
+    @balance
   end
 end
