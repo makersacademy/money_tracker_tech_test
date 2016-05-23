33b80758317343623e6ee555420f775583695df1
Anne <annemarie.k89@gmail.com>
2016-05-23 09:56:27 +0100

    moved transactions array into Statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,14 +1,9 @@
 class Statement
-  STATEMENT_HEAD = "date || credit || debit || balance"
-  def initialize(account_instance)
-    @account_instance = account_instance
+  attr_reader :transactions
+
+  def initialize
+    @transactions = []
   end
 
-  def print_statement
-    puts STATEMENT_HEAD
-    @account_instance.transactions.reverse.map do |trans|
-      puts "#{trans.date} || #{trans.credit} || #{trans.debit} || #{trans.current_balance}"
-    end
 
-  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,20 +1,10 @@
 require 'statement'
 
 describe Statement do
-  let(:transactions) { double :transactions }
+  subject(:statement) { described_class.new }
 
-  let(:balance) { double :balance }
-
-  let(:account) { double :account,
-                  balance: balance,
-                  transactions: transactions }
-
-  subject(:statement) { described_class.new(account) }
-
-  xdescribe '#print_statement' do
-    it 'shows all transactions' do
-
-    end
+  it 'initialises with an empty array of transactions' do
+    expect(statement.transactions).to eq []
   end
 
 end
