962c33e98a611ba0988bd36f1057b9792626efd6
Jojo <joseph.sharratt@gmail.com>
2016-05-23 10:04:05 +0100

    Starting again

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,11 +1,3 @@
 class Transaction
 
-  attr_reader :amount
-  attr_reader :date
-
-  def initialize(amount, date)
-    @amount = amount
-    @date = date
-  end
-
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,19 +2,5 @@ require 'transaction'
 
 describe Transaction do
 
-  # let(:subject) { described_class.new(amount,date) }
-  # subject(:transaction) { described_class.new }
-
-  before(:each) do
-    @transaction = described_class.new(1000, '10-01-2012')
-  end
-
-  it 'is initialized with an amount' do
-    expect(@transaction.amount).to eq 1000
-  end
-
-  it 'is initialized with a date' do
-    expect(@transaction.date).to eq '10-01-2012'
-  end
 
 end
