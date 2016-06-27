b5c01dc389d50e903a69c05024d9e485b70cf50a
Michael Harrison <harrim91@hotmail.co.uk>
2016-06-27 11:23:19 +0100

    set up angular, karma and protractor

---
+++ test/karma.conf.js
@@ -0,0 +1,37 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            "karma-spec-reporter"
+    ],
+
+    reporters: ["spec"],
+
+    specReporter: {
+      maxLogLines: 5,
+      suppressErrorSummary: true,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: true,
+      showSpecTiming: false
+    }
+
+  });
+};
