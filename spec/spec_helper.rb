6000f05751407efe8999d23ef569b34222fc1e90
Robin Heathcote <robin.heathcote@icloud.com>
2016-06-28 12:41:51 +0100

    Initial commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'coveralls'
+require 'simplecov'
+
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+# replace following line with SimpleCov.start to get coverage stats locally
+SimpleCov.start
+# run `open coverage/index.html` from the command line to view details
