14dab6249de8c055592b65036c64a6e88ff09e2e
Robin Heathcote <robin.heathcote@icloud.com>
2016-06-28 14:26:21 +0100

    add deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -3,4 +3,8 @@ class Account
   def initialize()
     @balance = 0
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
 end
--- spec/feature/account_spec.rb
+++ spec/feature/account_spec.rb
@@ -7,5 +7,9 @@ describe Account do
     it 'displays current balance' do
       expect(account.balance).to equal (0)
     end
+    it 'increases balance when a deposit is made' do
+      account.deposit(500)
+      expect(account.balance).to equal (500)
+    end
   end
 end
