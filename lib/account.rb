7ad9c53f3c6f4aa599a3b42c05c8cf3942de666d
RichardWatkins1 <richardwatkins1@gmail.com>
2016-06-28 11:35:00 +0100

    withdraw method added and tested

--- lib/account.rb
+++ lib/account.rb
@@ -10,4 +10,8 @@ class Account
     @balance += amount
   end
 
+  def withdraw(amount)
+    @balance -= amount
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,10 +6,19 @@ describe Account do
   subject(:account){ described_class.new }
 
   describe 'account balance' do
-    context 'it increases the accound balance when money is deposited' do
+    context 'it increases the account balance when money is deposited' do
       it 'increases the account balance by 1000 when 1000 is added' do
         account.deposit(1000)
         expect(account.balance).to eq (1000)
+      end
+    end
+
+    context 'it decreases the account balance when money is withdrawn' do
+      it 'decreases the account balance by 500 when 500 is added' do
+        account.deposit(1000)
+        account.deposit(2000)
+        account.withdraw(500)
+        expect(account.balance).to eq (2500)
       end
     end
   end
