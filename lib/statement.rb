2729aaa1dbaaa37e4891b59e485bdcf7dab619e8
RichardWatkins1 <richardwatkins1@gmail.com>
2016-06-28 16:23:30 +0100

    tests passing printer prints hash under the title

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,9 +1,22 @@
 class Statement
 
-  attr_reader :account
+  attr_reader :history, :account, :title
 
   def initialize(account)
     @account = account
+    @title = "date || credit || debit || balance\n"
+  end
+
+  def print_out
+    @history.each do |entry|
+      entry[:date].to_s + entry[:amount].to_s + entry[:balance].to_s + "\n"
+    end
+    print @title
+    print @history
+  end
+
+  def add_history(account)
+    @history = account.statement_history
   end
 
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -2,14 +2,25 @@ require "statement"
 
 describe Statement do
 
-  subject(:statement){ described_class.new(account) }
+  subject(:printed_statement){ described_class.new(account) }
 
   let(:account) {double :account, :balance => 0}
+  let(:statement) { [
+        {date: Time.local(2012, 1, 10), amount: 1000, balance: 1000},
+        {date: Time.local(2012, 1, 13), amount: 2000, balance: 3000},
+        {date: Time.local(2012, 1, 14), amount: -500, balance: 2500}
+      ] }
 
   describe 'printing account statement history' do
     context 'it initializes with an account that has a balance of 0' do
       it 'initializes with an account balance of 0' do
-        expect(statement.account.balance).to eq (0)
+        expect(printed_statement.account.balance).to eq (0)
+      end
+    end
+
+    context 'printing out the statement' do
+      it 'prints the headers if there is no history available' do
+        expect(printed_statement.title).to eq "date || credit || debit || balance\n"
       end
     end
   end
