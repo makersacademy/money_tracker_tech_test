e695263e3da0f3dc1a55e4d85b1fa3227778cc63
Richard Rodriguez <richard.rodriguez@comparethemarket.com>
2016-08-02 19:23:14 +0100

    Project setup and Travis CI configuring. Pushing one passing and one failing test

---
+++ MakersAcademy.Bank.Tests/JustSettingUp.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace MakersAcademy.Bank.Tests
+{
+    [TestFixture]
+    public class JustSettingUp
+    {
+        [Test]
+        public void DeleteMe()
+        {
+            var result = 1 + 1;
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void DeleteMeToo()
+        {
+            var result = 1 + 1;
+            Assert.That(result, Is.EqualTo(1));
+        }
+    }
+}
