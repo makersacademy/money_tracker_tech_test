8b8e13344b30790cc0b162d7a270de629ad351a6
Tim <timchipperfield@gmail.com>
2016-08-10 10:11:54 +0100

    initial commit

---
+++ spec/account_spec.rb
@@ -0,0 +1,11 @@
+describe "account" do
+  account = new Account
+
+   it "starts with a balance of 0" do
+     expect(account.balance).to eq 0
+  end
+
+  it "accepts a transaction" do
+    
+  end
+end
