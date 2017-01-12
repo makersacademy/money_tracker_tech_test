a1191b6aefc3c1fc48ba0038ffd017230cd805b9
paulwallis42 <paulwallis42@gmail.com>
2017-01-12 17:10:21 +0000

    deposit statement creation test passes

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,3 +1,21 @@
 # Class that creates statements for Account
 class Statement
+  attr_reader :account
+
+  def initialize(account)
+    @account = account
+  end
+
+  def print_statement
+    i = 0
+    while i < account.deposit_record.length do
+      transaction = account.deposit_record[i]
+      transaction_object = transaction[i]
+      date = transaction_object.created_date
+      value = transaction_object.value
+      balance = transaction[i+1]
+      puts "Date: #{date} Transaction Type: #{transaction_object.class} Amount: #{value} Balance: #{balance}"
+      i += 1
+    end
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe Statement do
+
+  let!(:deposit) do
+    double('deposit', value: 100,
+          created_date: Date.today.to_s)
+  end
+
+  let!(:withdrawl) do
+    double('withdrawl', value: 50,
+          created_date: Date.today.to_s)
+  end
+
+  let!(:account) do
+    double('account', deposit_record: [[deposit, 100]],
+          withdrawl_record: [[withdrawl, 50]])
+  end
+
+  let!(:statement) do
+    Statement.new(account)
+  end
+
+  it 'takes an account on creation' do
+    expect(statement.account).to eq(account)
+  end
+
+  it 'can output transactions' do
+    expect{ statement.print_statement }.to output(
+    "Date: 2017-01-12 Transaction Type: RSpec::Mocks::Double Amount: 100 Balance: 100\n"
+    ).to_stdout
+  end
 end
