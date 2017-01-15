24fa1e6b069aa19282158895cb741efaadf8827e
paulwallis42 <paulwallis42@gmail.com>
2017-01-15 18:07:41 +0000

    rectified mistake in statement tests

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -27,20 +27,24 @@ describe Statement do
 
   it 'can output all transactions' do
     expect(statement.print_statement).to eq(
-      ['Date: 2017-01-15  Transaction Type: Deposit  Amount: 100 Balance: 100',
-       'Date: 2017-01-15  Transaction Type: Withdrawl  Amount: 50 Balance: 50']
+      ["Date: #{Date.today}  Transaction Type: Deposit  Amount: 100 Balance: 100",
+       "Date: #{Date.today}  Transaction Type: Withdrawl  Amount: 50 Balance: 50"]
     )
   end
 
   it 'can filter just deposits' do
     expect(statement.print_deposits).to eq(
-      ['Date: 2017-01-15  Transaction Type: Deposit  Amount: 100 Balance: 100']
+      ["Date: #{Date.today}  Transaction Type: Deposit  Amount: 100 Balance: 100"]
     )
   end
 
   it 'can filter just withdrawls' do
     expect(statement.print_withdrawls).to eq(
-      ['Date: 2017-01-15  Transaction Type: Withdrawl  Amount: 50 Balance: 50']
+      ["Date: #{Date.today}  Transaction Type: Withdrawl  Amount: 50 Balance: 50"]
     )
+  end
+
+  it 'can output in date ascending order' do
+
   end
 end
