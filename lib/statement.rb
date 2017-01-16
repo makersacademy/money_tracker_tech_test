98a3602b1e953774cbf6cd2d128f85c20d441b5e
paulwallis42 <paulwallis42@gmail.com>
2017-01-16 09:34:18 +0000

    refactoring to statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -7,6 +7,20 @@ class Statement
   end
 
   def print_statement(order='ascending')
+    order == 'descending' ? full_statement_collation.reverse : full_statement_collation
+  end
+
+  def print_deposits(order='ascending')
+    order == 'descending' ? deposits_collation.reverse : deposits_collation
+  end
+
+  def print_withdrawls(order='ascending')
+    order == 'descending' ? withdrawls_collation.reverse : withdrawls_collation
+  end
+
+  private
+
+  def full_statement_collation
     i = 0
     print_array = []
     while i < account.transaction_record.length
@@ -17,62 +31,38 @@ class Statement
       " Amount: #{transaction_object.value} Balance: #{transaction[1]}")
       i += 1
     end
-    if order == 'descending'
-      return print_array.reverse
-    else
-      return print_array
-    end
-  end
-
-  def print_deposits
-    i = 0
-    print_array = []
-    while i < deposits_collation.length
-      print_array.push("Date: #{deposits_collation[i][0].created_date} " \
-      " Transaction Type: #{deposits_collation[i][0].class} " \
-      " Amount: #{deposits_collation[i][0].value} " \
-      "Balance: #{deposits_collation[i][1]}")
-      i += 1
-    end
     print_array
   end
-
-  def print_withdrawls
-    i = 0
-    print_array = []
-    while i < withdrawls_collation.length
-      print_array.push("Date: #{withdrawls_collation[i][0].created_date} " \
-      " Transaction Type: #{withdrawls_collation[i][0].class} " \
-      " Amount: #{withdrawls_collation[i][0].value} " \
-      "Balance: #{withdrawls_collation[i][1]}")
-      i += 1
-    end
-    print_array
-  end
-
-  private
 
   def deposits_collation
     i = 0
-    deposits_array = []
+    print_array = []
     while i < account.transaction_record.length
       if account.transaction_record[i][0].class == Deposit
-        deposits_array.push(account.transaction_record[i])
+        transaction = account.transaction_record[i]
+        transaction_object = transaction[0]
+        print_array.push("Date: #{transaction_object.created_date} " \
+        " Transaction Type: #{transaction_object.class} " \
+        " Amount: #{transaction_object.value} Balance: #{transaction[1]}")
       end
       i += 1
     end
-    deposits_array
+    print_array
   end
 
   def withdrawls_collation
     i = 0
-    withdrawls_array = []
+    print_array = []
     while i < account.transaction_record.length
       if account.transaction_record[i][0].class == Withdrawl
-        withdrawls_array.push(account.transaction_record[i])
+        transaction = account.transaction_record[i]
+        transaction_object = transaction[0]
+        print_array.push("Date: #{transaction_object.created_date} " \
+        " Transaction Type: #{transaction_object.class} " \
+        " Amount: #{transaction_object.value} Balance: #{transaction[1]}")
       end
       i += 1
     end
-    withdrawls_array
+    print_array
   end
 end
