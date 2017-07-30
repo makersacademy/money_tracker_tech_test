f6e18ed84efd2006ebb8b2bb676cc4457aa262fe
cyberplanner <sal@salweb.co.uk>
2017-07-30 16:22:56 +0100

    Add deposit function to Account, refactor code

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,11 @@
 class Account
-  def balance
-    0
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(amount)
+    @balance += amount
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,9 @@ describe Account do
   it "has a starting balance of 0" do
     expect(subject.balance).to eq(0)
   end
+
+  it "can make a deposit" do
+    subject.deposit(10)
+    expect(subject.balance).to eq(10)
+  end
 end
