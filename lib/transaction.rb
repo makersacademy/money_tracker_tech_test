dba1499d21f848dc8d8496a7af37e79498ce84f7
cyberplanner <sal@salweb.co.uk>
2017-07-30 16:44:52 +0100

    Add amount property to Transactions

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,6 @@
 class Transaction
-
+  attr_reader :amount
+  def initialize
+    @amount = 0
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,5 +1,7 @@
 require 'transaction'
 
 describe Transaction do
-
+  it "has a an amount" do
+    expect(subject.amount).to eq(0)
+  end
 end
