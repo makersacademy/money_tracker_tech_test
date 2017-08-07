592208f0a96902a97b88c09a12274e39f85af7b6
MarySalemme <marysalemme1@gmail.com>
2017-08-07 12:40:03 +0100

    Implementing methods to record debit and credit money

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -7,4 +7,19 @@ class Tracker
     @debit = 0
     @credit = 0
   end
+
+  def record_debit(amount)
+    @debit = amount
+    set_balance(-amount)
+  end
+
+  def record_credit(amount)
+    @credit = amount
+    set_balance(amount)
+  end
+
+  private
+    def set_balance(amount)
+      @balance += amount
+    end
 end
--- spec/tracker_spec.rb
+++ spec/tracker_spec.rb
@@ -14,4 +14,29 @@ describe Tracker do
   it "is initialised with a 0 credit" do
     expect(tracker.credit).to eq 0
   end
+
+  describe "#record_debit" do
+    it "updates the user's debit" do
+      tracker.record_debit(200)
+      expect(tracker.debit).to eq 200
+    end
+
+    it "updates the user's balance" do
+      tracker.record_debit(500)
+      expect(tracker.balance).to eq -500
+    end
+  end
+
+  describe "#record_credit" do
+    it "updates the user's credit" do
+      tracker.record_credit(500)
+      expect(tracker.credit).to eq 500
+    end
+
+    it "updates the user's balance" do
+      tracker.record_credit(500)
+      expect(tracker.balance).to eq 500
+    end
+  end
+
 end
