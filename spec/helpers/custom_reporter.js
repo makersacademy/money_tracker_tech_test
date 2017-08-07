26bb0e4baea99c43af5f52e4ac629df823251bf2
Corina <fluturelcorina@gmail.com>
2017-08-07 18:49:48 +0100

    Adding helper to include test descriptions when tests are run

---
+++ spec/helpers/custom_reporter.js
@@ -0,0 +1,26 @@
+// var myCustomReporter = {
+//   jasmineStarted: function (suiteInfo) {
+//     console.log('Running suite with ' + suiteInfo.totalSpecsDefined);
+//     console.log('Reporting via MyCustomReporter');
+//   },
+//
+//   suiteStarted: function (result) {
+//     console.log('Suite started: ' + result.description + ' whose full description is: ' + result.fullName);
+//   },
+//
+//   specStarted: function (result) {
+//     console.log('Spec started: ' + result.description + ' whose full description is: ' + result.fullName);
+//   },
+//
+//   specDone: function (result) {
+//   },
+//
+//   suiteDone: function (result) {
+//   },
+//
+//   jasmineDone: function () {
+//     console.log('Finished suite');
+//   }
+// };
+//
+// jasmine.getEnv().addReporter(myCustomReporter);
