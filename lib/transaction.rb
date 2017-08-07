7303a83110c304b53ca1ef5e80642fd4c2581193
Jasper King-Harman <j.kingharman@googlemail.com>
2017-08-07 16:04:34 +0100

    Adding transaction class and spec for its state

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,1 +1,13 @@
 
+class Transaction
+
+  attr_reader :credit, :debit, :time, :balance
+
+  def initialize(credit: 0, debit: 0, time: Time.now, balance: current_balance)
+    @credit = credit
+    @debit = debit
+    @time = time
+    @balance = balance
+  end
+
+end
---
+++ spec/transaction_spec.rb
@@ -0,0 +1,28 @@
+
+describe Transaction do
+
+  let(:credit) { 0 }
+  let(:debit) { 50 }
+  let(:balance) { 100 }
+  let(:current_time) { Time.now }
+  subject(:transaction) { described_class.new(credit: credit, time: current_time, debit: debit, balance: balance) }
+
+  describe "attributes" do
+    context "on init" do
+      it "will assign the credited amount" do
+        expect(transaction.credit).to eq(0)
+      end
+      it "will assign the debited amount" do
+        expect(transaction.debit).to eq(50)
+      end
+      it "will assign the credited amount" do
+        expect(transaction.balance).to eq(100)
+      end
+      it "will assign the credited amount" do
+        expect(transaction.time).to eq(current_time)
+      end
+    end
+  end
+
+
+end
