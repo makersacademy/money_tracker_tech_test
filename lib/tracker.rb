1dd1fdec2ae3c3834e9c450bc3a9db0ee71ecf22
MarySalemme <marysalemme1@gmail.com>
2017-08-07 11:40:52 +0100

    Initializing tracker class

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -1,3 +1,10 @@
 class Tracker
 
+  attr_reader :balance, :debit, :credit
+
+  def initialize
+    @balance = 0
+    @debit = 0
+    @credit = 0
+  end
 end
--- spec/tracker_spec.rb
+++ spec/tracker_spec.rb
@@ -1,4 +1,17 @@
 require 'tracker.rb'
 
 describe Tracker do
+  subject(:tracker) { described_class.new }
+
+  it "is initialized with a 0 balance" do
+    expect(tracker.balance).to eq 0
+  end
+
+  it "is initialised with a 0 debit" do
+    expect(tracker.debit).to eq 0
+  end
+
+  it "is initialised with a 0 credit" do
+    expect(tracker.credit).to eq 0
+  end
 end
