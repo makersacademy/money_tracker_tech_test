eaf84da1520feb1dbcbae99067288298173ac792
MarySalemme <marysalemme1@gmail.com>
2017-08-07 17:02:48 +0100

    Adding transactions history

--- lib/tracker.rb
+++ lib/tracker.rb
@@ -2,21 +2,25 @@ require_relative 'transaction'
 require 'date'
 
 class Tracker
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def record_debit(amount, transaction)
     transaction.set_debit(-amount)
     set_balance(-amount)
+    transaction.get_current_balance(@balance)
+    @transactions << transaction
   end
 
   def record_credit(amount, transaction)
     transaction.set_credit(amount)
     set_balance(amount)
     transaction.get_current_balance(@balance)
+    @transactions << transaction
   end
 
   private
--- spec/tracker_spec.rb
+++ spec/tracker_spec.rb
@@ -7,10 +7,19 @@ describe Tracker do
     expect(tracker.balance).to eq 0
   end
 
+  it "is initialized with an empty array of transactions" do
+    expect(tracker.transactions).to be_empty
+  end
+
   describe "#record_debit" do
     it "updates the user's balance after a withdrawal" do
       tracker.record_debit(500, transaction = Transaction.new)
       expect(tracker.balance).to eq -500
+    end
+
+    it "adds the withdrawal to the transactions history" do
+      tracker.record_debit(500, transaction = Transaction.new)
+      expect(tracker.transactions).to_not be_empty
     end
   end
 
@@ -18,6 +27,11 @@ describe Tracker do
     it "updates the user's balance after deposit" do
       tracker.record_credit(500, transaction = Transaction.new)
       expect(tracker.balance).to eq 500
+    end
+
+    it "adds the deposit to the transactions history" do
+      tracker.record_credit(500, transaction = Transaction.new)
+      expect(tracker.transactions).to_not be_empty
     end
   end
 end
