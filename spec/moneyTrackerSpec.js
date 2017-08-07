09bb8235adbfb7b5f701cd07fd68c75735cecdec
Miho Tse <mihotse@gmail.com>
2017-08-07 11:55:02 +0100

    Implement statement

--- spec/moneyTrackerSpec.js
+++ spec/moneyTrackerSpec.js
@@ -3,7 +3,7 @@ describe("Moneytracker", function() {
   var moneytracker;
 
   beforeEach(function() {
-    moneytracker = new Moneytracker();
+    moneytracker = new Moneytracker(0, 0, 0, 0);
   });
 
   describe("#balance", function() {
@@ -19,11 +19,20 @@ describe("Moneytracker", function() {
     });
   });
 
-  describe("#withdrawal", function() {
+  describe("#withdraw", function() {
     it("allows the user to withdraw money", function() {
       moneytracker.balance = 100;
-      moneytracker.withdrawal(50);
+      moneytracker.withdraw(50);
       expect(moneytracker.balance).toEqual(50);
+    });
+  });
+
+  describe("#statement", function() {
+    it("prints a statement", function() {
+      moneytracker.deposit(1000);
+      moneytracker.withdraw(250);
+      expect(moneytracker.balance).toEqual(750);
+      expect(moneytracker.statement).not.toBeNull();
     });
   });
 });
--- src/moneyTracker.js
+++ src/moneyTracker.js
@@ -1,13 +1,22 @@
-function Moneytracker() {
+function Moneytracker(date, credit, debit, balance) {
+  this.date = date;
+  this.credit = 0;
+  this.debit = 0;
   this.balance = 0;
 };
 
 Moneytracker.prototype.deposit = function (money) {
+  this.credit += money;
   this.balance += money;
   this._printDate + console.log(this.balance);
 };
 
-Moneytracker.prototype.withdrawal = function (money) {
+Moneytracker.prototype.withdraw = function (money) {
+  this.debit -= money;
   this.balance -= money;
   console.log(this.balance);
+};
+
+Moneytracker.prototype.statement = function () {
+  console.table([this]);
 };
