32ca0a6a92cf24a8d619afbba6c9e06ea0a09a71
Simon Hempenstall <simonjhemp@hotmail.com>
2017-08-07 12:22:31 +0100

    Record credit history with dates

--- lib/money_tracker.rb
+++ lib/money_tracker.rb
@@ -1,12 +1,15 @@
 class MoneyTracker
 
-  attr_reader :balance
+  attr_reader :balance, :credits
 
   def initialize(balance = 0)
     @balance = balance
+    @credits = []
   end
 
-  def credit(amount)
-    @balance += amount
+  def credit(amount, date)
+    format_amount = ('%.2f' % amount).to_f
+    @balance += format_amount
+    @credits << { date => format_amount }
   end
 end
--- spec/money_tracker_spec.rb
+++ spec/money_tracker_spec.rb
@@ -2,12 +2,23 @@ describe MoneyTracker do
 
   let(:money_tracker) { MoneyTracker.new }
 
+  it { is_expected.to respond_to(:credit).with(2).arguments }
+
   it "initialises with a balance of 0" do
     expect(money_tracker.balance).to eq 0
   end
 
-  it "can add credit to balance" do
-    money_tracker.credit(50)
-    expect(money_tracker.balance).to eq 50
+  describe "#credit" do
+    before do
+      money_tracker.credit(50,'07/08/17')
+    end
+
+    it "can add credit to the balance" do
+      expect(money_tracker.balance).to eq 50
+    end
+
+    it "can store date credit was made" do
+      expect(money_tracker.credits).to eq [{"07/08/17" => 50.0}]
+    end
   end
 end
