0bf293769572bf99e8b81d648da7fb8aa096c270
Jasper King-Harman <j.kingharman@googlemail.com>
2017-08-07 12:18:44 +0100

    Defining initial state for account class

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,14 @@
 
 class Account
+
+  INITIAL_BALANCE = 0
+
+  attr_reader :transactions, :balance
+
+  def initialize(statement, transactions)
+    @transactions = transactions || Transactions.new
+    @statement = statement || Statement
+    @balance = INITIAL_BALANCE
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,16 @@
 
 describe Account do
 
-  let(:statement) { double(:Statement) }
-  let(:transactions) { double(:Transactions) }
+  let(:statement) { double("Statement") }
+  let(:transactions) { double("Transactions") }
   subject(:account) { described_class.new(statement, transactions) }
+
+  describe "attributes" do
+    context "balance" do
+      it "will be set to zero" do
+        expect(account.balance).to eq(Account::INITIAL_BALANCE)
+      end
+    end
+  end
 
 end
