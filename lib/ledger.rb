a3b4ab5c5ad529e886c26fbb0057ba8eddbe8e30
jinimcoroneo <jinim1983@gmail.com>
2017-08-08 15:16:17 +0100

    refactor ledger class and remove all formatting

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -3,46 +3,15 @@ class Ledger
 
   def initialize
     @transactions = []
-    @balance = 0
+    @balance ||= 0
   end
 
-  def print_statement
-    print_heading
-    create_transaction_history
+  def add_record(record)
+    @transactions << record
   end
 
-  private
-
-  def print_heading
-    puts 'date || credit || debit || balance'
+  def update_balance
+    @balance = @transactions.map { |record| record.amount }.reduce(:+)
   end
 
-  def create_transaction_history
-    history = []
-    sort_transactions.each do |transaction|
-      history << if transaction[:type] == :debit
-                   format_debit(transaction)
-                 else
-                   format_credit(transaction)
-                 end
-    end
-    sort_transaction_history(history)
-    @balance = 0
-  end
-
-  def sort_transactions
-    @transactions.sort_by { |k, _v| k[:date] }
-  end
-
-  def format_debit(transaction)
-    "#{transaction[:date]} || || #{format('%.2f', transaction[:amount])} || #{format('%.2f', @balance -= transaction[:amount])}\n"
-  end
-
-  def format_credit(transaction)
-    "#{transaction[:date]} || #{format('%.2f', transaction[:amount])} || || #{format('%.2f', @balance += transaction[:amount])}\n"
-  end
-
-  def sort_transaction_history(transactions)
-    puts transactions.reverse.join
-  end
 end
--- spec/ledger_spec.rb
+++ spec/ledger_spec.rb
@@ -2,6 +2,8 @@ require './lib/ledger.rb'
 
 describe Ledger do
   subject(:my_ledger) { described_class.new }
+  let(:record1) { double(:record, amount: 100, date: "08/08/2017" ) }
+  let(:record2) { double(:record, amount: 250, date: "05/08/2017" ) }
 
   describe '#attributes' do
     it 'initializes with an empty transactions array' do
@@ -12,17 +14,31 @@ describe Ledger do
     end
   end
 
-  describe '#print_statement' do
-    it 'responds to the print_statement method' do
-      expect(my_ledger).to respond_to(:print_statement)
-    end
-    it 'allows me to print a full statement' do
-      my_ledger.transactions = [{ amount: 100, date: '07/08/2017', type: :credit }]
-      expect { my_ledger.print_statement }.to output("date || credit || debit || balance\n" + "07/08/2017 || 100.00 || || 100.00\n").to_stdout
-    end
-    it 'allows me to print a full statement in descending order of transaction' do
-      my_ledger.transactions = [{ amount: 100, date: '07/08/2017', type: :credit }, { amount: 200, date: '17/08/2017', type: :credit }]
-      expect { my_ledger.print_statement }.to output("date || credit || debit || balance\n" + "17/08/2017 || 200.00 || || 300.00\n07/08/2017 || 100.00 || || 100.00\n").to_stdout
+  describe '#add_record' do
+    it "adds a record to the transaction array" do
+      expect { my_ledger.add_record(record1) }.to change { my_ledger.transactions.length }.by(1)
     end
   end
+
+  describe '#update_balance' do
+    it "checks the balance of the ledger" do
+      my_ledger.add_record(record1)
+      my_ledger.add_record(record2)
+      expect(my_ledger.update_balance).to eq(350)
+    end
+   end
+
+  # describe '#print_statement' do
+  #   it 'responds to the print_statement method' do
+  #     expect(my_ledger).to respond_to(:print_statement)
+  #   end
+  #   it 'allows me to print a full statement' do
+  #     my_ledger.transactions = [{ amount: 100, date: '07/08/2017', type: :credit }]
+  #     expect { my_ledger.print_statement }.to output("date || credit || debit || balance\n" + "07/08/2017 || 100.00 || || 100.00\n").to_stdout
+  #   end
+  #   it 'allows me to print a full statement in descending order of transaction' do
+  #     my_ledger.transactions = [{ amount: 100, date: '07/08/2017', type: :credit }, { amount: 200, date: '17/08/2017', type: :credit }]
+  #     expect { my_ledger.print_statement }.to output("date || credit || debit || balance\n" + "17/08/2017 || 200.00 || || 300.00\n07/08/2017 || 100.00 || || 100.00\n").to_stdout
+  #   end
+  # end
 end
