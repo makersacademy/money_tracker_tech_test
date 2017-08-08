78c6e98f3c17f6414f2c374aa452a5f479db2fa9
MarySalemme <marysalemme1@gmail.com>
2017-08-08 13:01:54 +0100

    Adding formatting method to transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -8,7 +8,16 @@ class Transaction
     @amount = amount
   end
 
-  # def format_for_statement
-  #   p @date + " || " + @credit.to_s + " || " + @debit.to_s + " || " + @current_balance.to_s
-  # end
+  def format_for_statement
+    @date + " || " + check_if_credit_or_debit
+  end
+
+  private
+    def check_if_credit_or_debit
+      if @amount > 0
+        @amount.to_s + " || || "
+      else
+        "|| " + @amount.to_s + " || "
+      end
+    end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,21 +1,19 @@
 require 'transaction.rb'
 
 describe Transaction do
-  subject(:transaction) { described_class.new("02/08/2017", 300) }
+  subject(:transaction) { described_class.new("02/08/2017", -300) }
 
   it "is initialised with the date set by the user" do
-    p transaction
     expect(transaction.date).to eq "02/08/2017"
   end
 
   it "is initialised with the amount set by the user" do
-    expect(transaction.amount).to eq 300
+    expect(transaction.amount).to eq -300
   end
 
-  # describe "#format_for_statement" do
-  #   it "prints the transaction date, credit/debit amount and balance" do
-  #     tracker.record_credit(1200, transaction = Transaction.new)
-  #     expect(transaction.format_for_statement).to eq "07/08/2017 || 1200 || || 2500"
-  #   end
-  # end
+  describe "#format_for_statement" do
+    it "returns the formatted transaction date and amount" do
+      expect(transaction.format_for_statement).to eq "02/08/2017 || || -300 || "
+    end
+  end
 end
