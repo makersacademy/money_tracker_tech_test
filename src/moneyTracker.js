03271c5634de381a483d15cb988092b888876c85
Miho Tse <mihotse@gmail.com>
2017-08-09 23:03:56 +0100

    Separate Statement from Moneytracker

--- src/moneyTracker.js
+++ src/moneyTracker.js
@@ -18,28 +18,8 @@ Moneytracker.prototype._updateTotalBalance = function () {
   };
 };
 
-// Moneytracker.prototype.printStatement = function () {
-//   this._updateTotalBalance();
-//   return this._createStatementHeader() + this._createStatement();
-// };
-
 Moneytracker.prototype.printStatement = function () {
   this._updateTotalBalance();
   var statement = new Statement(this._transactions);
   return statement.create;
 };
-
-// Moneytracker.prototype._createStatement = function () {
-//   var statement = "";
-//   for(var i = 0; i < this._transactions.length; i++) {
-//     statement += (this._transactions[i].date) + " || " +
-//                  (this._transactions[i].credit) + " || " +
-//                  (this._transactions[i].debit) + " || " +
-//                  (this._transactions[i].balance) + "\n";
-//   };
-//   return statement;
-// };
-
-// Moneytracker.prototype._createStatementHeader = function () {
-//   return "date || credit || debit || balance\n";
-// };
---
+++ src/statement.js
@@ -0,0 +1,22 @@
+function Statement(transactions) {
+  this.create = this._createStatement(transactions);
+};
+
+Statement.prototype._createStatementBody = function (transactions) {
+  var statement = "";
+    for(var i = 0; i < transactions.length; i++) {
+      statement += (transactions[i].date) + " || " +
+                   (transactions[i].credit) + " || " +
+                   (transactions[i].debit) + " || " +
+                   (transactions[i].balance) + "\n";
+    };
+  return statement;
+};
+
+Statement.prototype._createStatementHeader = function () {
+  return "date || credit || debit || balance\n";
+};
+
+Statement.prototype._createStatement = function (transactions) {
+  return this._createStatementHeader() + this._createStatementBody(transactions);
+};
