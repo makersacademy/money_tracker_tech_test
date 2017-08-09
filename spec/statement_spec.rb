8e8d5aea429a6b40a1b3037d2918c470d94343d8
Alistair Kung <alistaircckung@gmail.com>
2017-08-09 15:21:42 +0100

    completed statement class

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,15 +2,15 @@ require 'statement'
 
 describe Statement do
 
-  let(:transactions) { double('transactions') }
   let(:earning) { double('earning', date: Date.parse("7, Aug, 2017"), debit: 0, credit: 100) }
-  let(:spending) { double('spending', date: Date.parse("8, Aug, 2017"), debit: 50, credit: 0) }
+  let(:spending) { double('spending', date: Date.parse("7, Aug, 2017"), debit: 50, credit: 0) }
+  let(:transactions) { [earning, spending] }
 
-  subject(:statement) { described_class.new(transactions )}
+  subject(:statement) { described_class.new(transactions)}
 
   describe "#generate_statement" do
-    it "should print standard headers" do
-      expect { statement.generate_statement }.to output("date | debit | credit | balance\n").to_stdout
+    it "should display the statement" do
+      expect { statement.generate_statement }.to output("date | debit | credit | balance\n2017-08-07 | 0 | 100 | 100\n2017-08-07 | 50 | 0 | 50\n").to_stdout
     end
   end
 end
